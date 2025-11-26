91381030f873789f521288d3841cf5c4f8678303
Yu Jiheon <luv.wlgjs@gmail.com>
2025-11-26 11:26:43 +0900

    test: StudyplatformApplicationTests 추가
    - Spring Boot 기본 컨텍스트 로드 테스트
    - @SpringBootTest 어노테이션

---
+++ src/test/java/org/study/platform/StudyplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
