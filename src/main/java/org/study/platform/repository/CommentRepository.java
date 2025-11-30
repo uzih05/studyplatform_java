7f95a24f5c719fa368735f51bbd53c29d26fb370
Yu Jiheon <luv.wlgjs@gmail.com>
2025-11-30 15:17:26 +0900

    feat(repository): CommentRepository 추가
     - Comment 엔티티용 Repository 인터페이스 추가
     - 게시물별 댓글 조회 및 정렬 메서드 포함

--- src/main/java/org/study/platform/repository/CommentRepository.java
+++ src/main/java/org/study/platform/repository/CommentRepository.java
@@ -7,13 +7,7 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    // 특정 게시글의 모든 댓글 조회
     List<Comment> findByPostId(Long postId);
-
-    // 특정 게시글의 댓글을 생성일순으로 조회
     List<Comment> findByPostIdOrderByCreatedAtAsc(Long postId);
-
-    // 특정 작성자의 댓글 조회
     List<Comment> findByAuthorId(Long authorId);
 }
